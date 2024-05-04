fc069eb8c7a1b1435a54ec17b564ecbc89e0df08
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2024-05-04 18:42:37 +0530

    Initial Commit

---
+++ UpToDo/UpToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpToDoApp.swift
+//  UpToDo
+//
+//  Created by Ganpat Jangir on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct UpToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
